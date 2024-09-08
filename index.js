2f190c8d6a268ce36bae56c890a69b09b54ae9da
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-09-08 10:27:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let partha = "partha";
+var pratim ="pratim";
+const bhukta = "bhukta";
+
+console.log(partha, pratim,bhukta)
+partha = "a"
+pratim="b"
+//bhukta="c"
+console.log(partha, pratim,bhukta)
---
+++ newJS.js
@@ -0,0 +1,18 @@
+"use script";
+   
+
+const myName= "partha"// string
+let age = 18// number
+
+console.log(typeof age)
+
+/*typeof javascript
+string
+number
+bigint
+boolian
+null
+undefind
+array[]
+object{}
+*/
