ceda3520acbf0a42b05f8af2e4f8029a85e2c621
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-09-08 14:12:52 +0530

    done

---
+++ opperation.js
@@ -0,0 +1,17 @@
+console.log("1"+1+1+"1"+1+1);
+
+console.log(+"");
+
+let counter = 100;
+counter++
+console.log(counter);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%9)
+
+
+
