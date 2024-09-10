07902e0dd6b8a04225e30cf59e0fbc526a7897b1
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-09-10 08:31:28 +0530

    Done

---
+++ concatinate.js
@@ -0,0 +1,12 @@
+const Fname="partha";
+const lname="bhukta"
+
+console.log(`hi my self ${Fname} and my last name is ${lname}`)
+const str = new String("partha pratim bhukra")
+console.log(str)
+const balance = new Number (100);
+console.log(balance)
+console.log(Math)
+console.log(Math.round(7.5))
+console.log(Math.abs(-7.5))
+console.log(Math.random()*5+1)
---
+++ date.js
@@ -0,0 +1,5 @@
+const myDate = new Date()
+console.log(myDate.toJSON())
+
+const mynewdate =  Date.now("12-05-2024")
+console.log(mynewdate.toString())
