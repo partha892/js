e5c72a6a33562116e1d2e196d0cba97830fa8330
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-09-13 14:39:58 +0530

    switch.js

---
+++ switch.js
@@ -0,0 +1,39 @@
+const  month = "2"
+switch (month.length) {
+    case 1:
+        console.log("jan")
+        break;
+        case 2:
+            console.log("Feb")
+            break;
+            case 3:
+                console.log("march")
+                break;
+                case 4:
+                    console.log("april")
+                    break;
+                    case 5:
+                        console.log("may")
+                        break;
+
+    default:
+        console.log("rest of month")
+        break;
+}
+
+
+
+const arr = []
+const obj = {}
+
+if (arr.length === 0) {
+    console.log("empty Array")
+}
+
+if (Object.keys(obj).length === 0) {
+    console.log("empty object")
+}
+//nullish opperator ??
+
+let val = undefined ?? 889
+console.log(val)
